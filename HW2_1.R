db70385f855fc30210c286a3188208cf588c279d
garnerat <atg530@gmail.com>
2017-01-27 21:18:22 -0500

    linear reg without variable selection

--- HW2_1.R
+++ HW2_1.R
@@ -58,20 +58,89 @@ colnames(corr[, highCorr,drop = FALSE])
 ggcorr(df_cor, method = c("complete", "pearson"), nbreaks = 10) #could also use pairwise, which is the default
 
 # loop of boxplot based on example here: https://www.r-bloggers.com/ggplot2-graphics-in-a-loop/
-
+# need to fix axis titles and scale issues from outliers
 boxplot.loop <- function(x, na.rm = TRUE, ...) {
   numer <- sapply(x, is.numeric)
   nm <- names(x[,numer])
   for (i in seq_along(nm)) {
-    plots <-ggplot(data = x,aes(x = factor(0), y = nm[i])) + geom_boxplot()
-    ggsave(plots,filename=paste("boxplot",nm[i],".png",sep="_"))
+   plots <- ggplot(data = x,aes(x = factor(0), y = x[,nm[i]])) + geom_boxplot()
+   ggsave(plots,filename=paste("boxplot",nm[i],".png",sep="_"))
   }
 }
 
 boxplot.loop(train) 
 
 
-ggplot(data = train, aes(x = factor(0), y= nox)) + geom_boxplot() + coord_flip()
+#ggplot(data = train, aes(x = factor(0), y= crim)) + geom_boxplot() + coord_flip()
+
+##### Model with no variable manipulation #####
+
+linreg.model <- lm(medv ~ ., data = train)
+
+summary(linreg.model)
+
+#significant variables
+  # crim         
+  # zn          
+  # chas          
+  # nox       
+  # rm            
+  # dis          
+  # rad         
+  # tax          
+  # ptratio      
+  # black        
+  # lstat        
+
+# could standardize variable if inference from coefficients is desired,
+# would have to address multicollinearity also
+
+  # for (i in 1:(ncol(Boston_train) - 1)) {
+  # Boston_train[, i] = scale(Boston_train[, i])
+  # }
+
+# ways to add interaction effects
+  #lm(medv ~ crim + zn + crim:zn, data = Boston_train)
+  #lm(medv ~ crim * zn, data = Boston_train)
+
+# Model Fitness
+
+# MSE
+model_summary <- summary(linreg.model)
+(model_summary$sigma)^2
+
+# R-squared 
+
+model_summary$r.squared
+
+# Adjusted R-squared  (penalizes model complexity)
+model_summary$adj.r.squared
+
+# AIC and BIC of the model, these are information criteria. Smaller values indicate better fit.
+
+AIC(linreg.model)
+
+BIC(linreg.model)
+
+# Out of sample prediction
+
+# pi is a vector that contains predicted values for test set.
+
+pi <- predict(object = linreg.model, newdata = test)
+
+
+# Mean Squared Error (MSE): average of the squared differences between the predicted and actual values
+
+mean((pi - test$medv)^2)
+
+
+# Mean Absolute Error (MAE) (less popular)
+mean(abs(pi - test$medv))
+
+
+##### variable selection #####
+
+
 
 
 ###################################### Problem 1.2 ################################################
