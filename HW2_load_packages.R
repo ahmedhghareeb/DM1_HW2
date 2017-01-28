553db310b82154a43c7929d7e2a165f82212a86e
garnerat <atg530@gmail.com>
2017-01-27 19:28:23 -0500

    initial commit

---
+++ HW2_load_packages.R
@@ -0,0 +1,13 @@
+# Data Mining I
+# HW 2
+# Linear Regression
+# Boston Housing Data
+
+##### install packages #####
+
+
+packages <- c("MASS","ggplot2","GGally","rpart","caret")
+
+install.packages(packages)
+
+lapply(as.list(packages),library, character.only = TRUE)
